92026a02c3295598eaa3c6f705ce6a5c1e77d692
Toan Nguyen <nntoan@protonmail.com>
2015-12-26 10:18:53 +0700

    Update BypassHighlandCoffee to 0.1.2

--- BypassHighlandCoffee.user.js
+++ BypassHighlandCoffee.user.js
@@ -5,14 +5,16 @@
 // @author      Moriarty
 // @include     http://*
 // @include     https://*
-// @version     0.1.1
+// @version     0.1.2
 // @license	GNU/GPL v3
 // @grant       none
 // ==/UserScript==
-var isHighlandCoffee = (document.forms[0].action.indexOf('/nodogsplash_auth/') > - 1) ? true : false;
+var docTitle = document.title;
+var searchText = 'Viet Thai International | Connect';
+var isHighlandCoffee = (docTitle === searchText) ? true : false;
 if (isHighlandCoffee) {
     console.info('Fucking HighlandCoffee HotSpot...');
-    document.getElementsByName('redir') [0].value = window.location.href;
+    document.getElementsByName('redir')[0].value = window.location.href;
     document.forms[0].submit();
 } else {
     console.warn('There is no hotspot to bypass, fuck Highland!');
